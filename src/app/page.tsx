9246e643556d8dc2848efe465567197e3ec0dc0a
Alexandr <alex-demchenko@users.noreply.github.com>
2025-11-06 23:20:15 +0200

    add icon for article

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ScrollText } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -5,8 +6,11 @@ export default function Home() {
     <div>
       <p>Рекомедуемая последовательность изучения Буддизма:</p>
       <p>
-        <Link href="/ru/articles/varieties-of-buddhism">
-          Разновидности Буддизма
+        <Link
+          href="/ru/articles/varieties-of-buddhism"
+          className="flex items-center gap-2"
+        >
+          <ScrollText /> Разновидности Буддизма
         </Link>
       </p>
     </div>
