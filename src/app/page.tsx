f16c13b66e5c75f421978cfe3729387665570eb6
Alexandr <alex-demchenko@users.noreply.github.com>
2025-12-05 22:01:12 +0200

    content: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
-import { ScrollText } from "lucide-react";
-import Link from "next/link";
+import Content from "./content.mdx";
 
 export default function Home() {
-  return (
-    <div>
-      <p>Рекомедуемая последовательность изучения Буддизма:</p>
-      <p>
-        <Link
-          href="/ru/articles/varieties-of-buddhism"
-          className="inline-flex items-center gap-2"
-        >
-          <ScrollText /> Разновидности Буддизма
-        </Link>
-      </p>
-    </div>
-  );
+  return <Content />;
 }
 
