5c4b589d35ddbd847b3cc7c28a4e51b1645bc3d9
Alexandr <alex-demchenko@users.noreply.github.com>
2026-01-30 16:07:00 +0200

    fix: path to file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import { readFileSync } from "node:fs";
 
 export default function Home() {
-  const content = readFileSync(
-    "D:/Work/GitHub/dharma.github.io/src/app/content.html"
-  ).toString();
+  const content = readFileSync("./src/app/content.html").toString();
 
   return <div dangerouslySetInnerHTML={{ __html: content }} />;
 }
