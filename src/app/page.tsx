8aad37bd6bd5245e79e3c5ee8166a5bac20ec1bf
Alexandr <alex-demchenko@users.noreply.github.com>
2026-02-08 10:13:07 +0200

    feat: warning message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import { CircleAlert } from "lucide-react";
 import { readFileSync } from "node:fs";
 
 export default function Home() {
   const content = readFileSync("./src/app/content.html").toString();
 
-  return <div dangerouslySetInnerHTML={{ __html: content }} />;
+  return (
+    <div>
+      <div className="p-4 border border-gray-200 rounded-lg bg-yellow-100">
+        <div className="flex items-center gap-2">
+          <CircleAlert />
+          <div>
+            The algorithm of content arrangement is in development and will be
+            improved.
+          </div>
+        </div>
+      </div>
+
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  );
 }
 
